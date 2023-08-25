be057f5aaf3dc9a84a9d9924e152c478becf93c6
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2023-08-25 15:37:36 +0200

    Complete data aggregation ad preprocessing files

---
+++ data/Data mining and preprocessing.py
@@ -0,0 +1,60 @@
+from features import qualityFeatures,relativeAggregation,goalScoredFeatures
+import pandas as pd
+from sklearn.feature_selection import VarianceThreshold
+
+# Aggregates quality features (e.g. Accurate/Inaccurate simple passes, Headers, Lanuches, Assists, Dangerous lost balls...)
+qualityFeat = qualityFeatures.qualityFeatures()
+data_folder_path = "your\folder\path\to\figshare_downloads\from\'A Dataset for football, Cintia-Pappalardo'\\"
+quality=qualityFeat.createFeature(events_path = data_folder_path+'data/events/*.json',
+                        players_file=data_folder_path+'data/players.json', entity = 'team')
+# Aggregates the feature 'goal-scored' defined as the difference between goal scored and conceded ina a game
+gs=goalScoredFeatures.goalScoredFeatures()
+goals=gs.createFeature(data_folder_path+'data/matches/*.json')
+
+# Merging of quality features and goals scored
+aggregation = relativeAggregation.relativeAggregation()
+aggregation.set_features([quality,goals])
+df = aggregation.aggregate(to_dataframe = True)
+df["goal-scored"] = df.pop("goal-scored") # brings the goal-score column at the end
+
+class FeaturesPreprocessing():
+    """
+    Aggregated data preprocessing.
+
+    Parameters
+    ----------
+        dataframe : pandas DataFrame
+            a dataframe containing the feature values and the target values
+
+        target: str
+            a string indicating the name of the target variable in the dataframe
+    """
+    def __init__(self, dataframe, target, var_threshold = 0.02):
+        ##feature selection by variance, to delete outlier features
+        feature_names = list(dataframe.columns)
+        # eliminate the variables with zero variance
+        sel = VarianceThreshold(var_threshold)
+        X = sel.fit_transform(dataframe)
+        # print the filtered out variables
+        selected_feature_names = [feature_names[i] for i, var in enumerate(list(sel.variances_)) if var > var_threshold]
+        filtered_features = [(feature_names[i],var) for i, var in enumerate(list(sel.variances_)) if var <= var_threshold]
+        print ("[Weighter] filtered features:", filtered_features, len(filtered_features))
+        # create the final dataframe
+        self.dataframe = pd.DataFrame(X, columns=selected_feature_names)
+        # create the labels Series
+        y = self.dataframe[target].apply(lambda x: 1 if x > 0 else -1)
+        # Add the label column to the dataframe and rename it ('goal-scored' disappears)
+        self.dataframe[target] = y
+        self.dataframe.rename({target: 'victory-defeat'}, axis=1, inplace=True)
+        # Get the matrix format of X and y
+        self.X = self.dataframe.loc[:, self.dataframe.columns != 'victory-defeat'].values
+        self.y = y.values
+    def return_dataframe(self):
+        return self.dataframe
+    def return_X(self):
+        return self.X
+    def return_y(self):
+        return self.y
+
+fetch_data = FeaturesPreprocessing(dataframe = df, target = "goal-scored", var_threshold = 0.02)
+# fetch_data.return_dataframe().to_csv("aggregated_data.csv")
---
+++ data/features/qualityFeatures.py
@@ -0,0 +1,110 @@
+from .abstract import Feature
+from .wyscoutEventsDefinition import *
+import json
+from collections import defaultdict
+import glob
+
+
+class qualityFeatures(Feature):
+    """
+    Quality features are the count of events with outcomes.
+    E.g.
+    - number of accurate passes
+    - number of wrong passes
+    ...
+    """
+    def createFeature(self,events_path,players_file,entity = 'team',select = None):
+        """
+        compute qualityFeatures
+        parameters:
+        -events_path: file path of events file
+        -select: function  for filtering events collection. Default: aggregate over all events
+        -entity: it could either 'team' or 'player'. It selects the aggregation for qualityFeatures among teams or players qualityfeatures
+
+        Output:
+        list of dictionaries in the format: matchId -> entity -> feature -> value
+        """
+        event2subevent2outcome={
+                1:{10: [1801, 1802],
+                 11: [1801, 1802],
+                 12: [1801, 1802],
+                 13: [1801, 1802]},
+                 2: [1702, 1703, 1701], #fouls aggregated into macroevent
+
+                 3 :{30: [1801, 1802],
+                 31: [1801, 1802],
+                 32: [1801, 1802],
+                 33: [1801, 1802],
+                 34: [1801, 1802],
+                 35: [1802],
+                 36: [1801, 1802]},
+                 4: {40: [1801, 1802]},
+                 6: {60: []},
+                 7: {70: [1801, 1802,101],
+                 71: [1801, 1802,101],
+                 72: [1401, 1302, 201, 1901, 1301, 2001, 301]},
+                 8: {80: [1801, 1802,302,301],
+                 81: [1801, 1802,302,301],
+                 82: [1801, 1802,302,301],
+                 83: [1801, 1802,302,301],
+                 84: [1801, 1802,302,301],
+                 85: [1801, 1802,302,301],
+                 86: [1801, 1802,302,301]},
+                 #90: [1801, 1802],
+                 #91: [1801, 1802],
+                 10: {100: [1801, 1802]}}
+
+        aggregated_features = defaultdict(lambda : defaultdict(lambda: defaultdict(int)))
+
+        players =  json.load(open(players_file))
+        #  filtering out all the events from goalkeepers
+        goalkeepers_ids = [player['wyId'] for player in players
+                                if player['role']['name']=='Goalkeeper']
+
+        events = []
+        for file in glob.glob("%s"%events_path):
+            data = json.load(open(file))
+            if select:
+                data = list(filter(select,data))
+            events += list(filter(lambda x: x['matchPeriod'] in ['1H','2H'] and x['playerId'] not in  goalkeepers_ids,data)) #excluding penalties events
+            print ("[qualityFeatures] added %s events from %s"%(len(data), file))
+        
+
+        for evt in events:
+            if evt['eventId'] in event2subevent2outcome:
+                ent = evt['teamId'] #default
+                if entity == 'player':
+                    ent = evt['playerId']
+
+                evtName =evt['eventName']
+
+                if type(event2subevent2outcome[evt['eventId']]) == dict:
+                    #hierarchy as event->subevent->tags
+                    if evt['subEventId'] not in event2subevent2outcome[evt['eventId']]:
+                        #malformed events
+                        continue #skip to next event
+                    tags = [x for x in evt['tags'] if x['id'] in event2subevent2outcome[evt['eventId']][evt['subEventId']]]
+
+                    evtName+="-%s"%evt['subEventName']
+                else:
+                    #hierarchy as event->tags
+                    tags = [x for x in evt['tags'] if x['id'] in event2subevent2outcome[evt['eventId']]]
+
+                if len(tags)>0:
+                    for tag in tags:
+                        aggregated_features[evt['matchId']][ent]["%s-%s"%(evtName,tag2name[tag['id']])]+=1
+
+                else:
+                    aggregated_features[evt['matchId']][ent]["%s"%(evtName)]+=1
+        result =[]
+        for match in aggregated_features:
+            for entity in aggregated_features[match]:
+                for feature in aggregated_features[match][entity]:
+                    document = {}
+                    document['match'] = match
+                    document['entity'] = entity
+                    document['feature'] = feature
+                    document['value'] = aggregated_features[match][entity][feature]
+                    result.append(document)
+
+        return result
